c04b130af3718274b37b733e6620354da1c3d11c
David <davidgrong778@gmail.com>
2022-04-07 17:29:46 -0500

    files for project

---
+++ stories.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+
+/** Get and show stories when site first loads. */
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+/**
+ * A render method to render HTML for an individual Story instance
+ * - story: an instance of Story
+ *
+ * Returns the markup for the story.
+ */
+
+function generateStoryMarkup(story) {
+  // console.debug("generateStoryMarkup", story);
+
+  const hostName = story.getHostName();
+  return $(`
+      <li id="${story.storyId}">
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    `);
+}
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+
+  // loop through all of our stories and generate HTML for them
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  }
+
+  $allStoriesList.show();
+}
+
+async function storySubmit(evt){
+    evt.preventDefault();
+    const author = $("#create-author").text();
+    const title = $("#create-title").text();
+    const url = $("#create-url").text();
+    const username = currentUser.username;
+    const story = {
+        title,
+        url,
+        author,
+        username
+    }
+
+    const storyResponse = await storyList.addStory(currentUser, story);
+    const thisStory = generateStoryMarkup(storyResponse);
+    $allStoriesList.prepend(thisStory);
+
+}
